99bd7972864fbfcd62d50ef41a43878f6a98454e
Leo Mejia <leonardo.mejia@est.iudigital.edu.co>
2022-12-11 22:07:27 -0500

    :tada: firdt commit

---
+++ routes/usuario.js
@@ -0,0 +1,45 @@
+const { Router } = require('express')
+const { 
+    createUsuario, 
+    getUsuarios, 
+    getUsuarioByID,
+    updateUsuarioByID,
+    deleteUsuarioByID
+} = require('../controllers/usuario')
+
+const router = Router()
+const validarJwt = require('../middlewares/validarJwt')
+const { esAdmin } = require('../middlewares/validarRol')
+
+
+/**
+ * Crea un usuario
+ */
+router.post('/', [validarJwt,
+    esAdmin], createUsuario)
+
+/**
+ * Consulta todos los usuarios
+ */
+router.get('/', [validarJwt,
+    esAdmin], getUsuarios)
+
+/**
+ *  Consulta un usuario por su ID
+ */
+router.get('/:id', [validarJwt,
+    esAdmin], getUsuarioByID)
+
+/**
+ * Actualiza un usuario por su ID
+ */
+router.put('/:id', [validarJwt,
+    esAdmin], updateUsuarioByID)
+
+/**
+ * Borra un usuario por su ID
+ */
+router.delete('/:id', validarJwt, esAdmin, deleteUsuarioByID)
+
+module.exports = router
+
